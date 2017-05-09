d6e1509ea49063e71aabef4529d88731bec9dd0d
krausbreanna4 <bkraus002@my.hennepintech.edu>
2017-05-08 21:06:55 -0500

    New Labels and Text

--- FinalProject/FinalProject/MainPage.xaml.cs
+++ FinalProject/FinalProject/MainPage.xaml.cs
@@ -31,11 +31,11 @@ namespace FinalProject
                     , "Fortune Not Found: Abort, Retry, Ignore?"
                     , "A good time to start something new"
                     , "I find your lack of faith disturbing."
-                    ,"It’s a trap!"
-                    ,"Do. Or do not. There is no try.”"
-                    ,"Your mother was a hamster and your father smelt of elderberries."
-                    ,"I fart in your general direction."
-                    ,""
+                    , "It’s a trap!"
+                    , "Do. Or do not. There is no try.”"
+                    , "Your mother was a hamster and your father smelt of elderberries."
+                    , "I fart in your general direction."
+
         };
 
 
@@ -43,18 +43,31 @@ namespace FinalProject
 
         private void Button_OnClick(object sender, EventArgs e)
         {
+            
             Button button = (Button)sender;
             if (button.Text == "Red" || button.Text == "Blue" || button.Text == "Green" || button.Text == "Yellow")
             {
+                colorOutput.Text = "Pick a Number.";
                 Button1.Text = "1";
                 Button2.Text = "2";
                 Button3.Text = "3";
                 Button4.Text = "4";
+
+                output.Text = "";
             }
+
+            
             else if (button.Text == "1" || button.Text == "2" || button.Text == "3" || button.Text == "4")
             {
+                
                 var rnd = new System.Random();
                 output.Text = options[rnd.Next(0, options.Length - 1)];
+
+                Button1.Text = "Red";
+                Button2.Text = "Blue";
+                Button3.Text = "Green";
+                Button4.Text = "Yellow";
+                colorOutput.Text = "Pick Again!";
             }
             
         }
